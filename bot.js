02414d573862ef47626fa7c83d03e1fe243a63a9
sonsilv222 <58892069+sonsilv222@users.noreply.github.com>
2019-12-14 21:23:02 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NjU1NDM3ODgyMzI0MDI1MzU1.XfUGWQ.CRggYTK7h3n8zm4ZSyRcO_6jyOQ'
+
+client.login(token);
+
+client.on('message', message => {
+    
+        if(message.content.toLowerCaser() === 'hello')
+            message.channel.send('I am a seagull' + message.author);
+
+        else if(message.content.toLowerCase() === 'Squawk')
+            message.reply('Squawk');
+});
+
+client.login(process.env.BOT_TOKEN);
